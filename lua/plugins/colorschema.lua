6cb3176462d4d600ffc4a76f0bc9f1d9f5fbb985
Sérgio Costa <sergio.md.costa@protonmail.com>
2026-05-03 22:20:10 +0100

    feat(colorscheme): add everforest and update colorscheme configuration

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,9 +1,27 @@
 return {
-  { "Mofiqul/dracula.nvim", name = "dracula", priority = 1000 },
+  {
+    "sainnhe/everforest",
+    name = "everforest",
+    priority = 1000,
+    init = function()
+      vim.o.background = "dark"
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "onedark",
     },
+  },
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "deep",
+      })
+      vim.cmd.colorscheme("onedark")
+    end,
   },
 }
